77770aae32293adfb492c4fd88c9112584c7a3f3
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2023-10-10 13:01:12 +0200

    Fixed the test (hopefully)

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const expect = chai.expect;
 describe('Node-RED Flow Tests', function () {
   it('should test the Node-RED flow', function (done) {
     // Replace this with the appropriate URL for your Node-RED instance
-    const nodeRedURL = 'http://localhost:1880';
+    const nodeRedURL = 'http://127.0.0.1:1880';
 
     // Replace this with the payload you want to inject in your flow
     const injectPayload = {
@@ -22,6 +22,7 @@ describe('Node-RED Flow Tests', function () {
       .end(function (err, res) {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
+        
 
         // Replace 'debug-node-output' with the name of your debug node
         // and 'expected-output' with the expected output you're looking for
